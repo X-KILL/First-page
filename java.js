6f53453c8dc2d6da7e9a5bae6f8682a76c94f0f6
X-KILL <161455423+X-KILL@users.noreply.github.com>
2024-09-03 21:23:02 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+//alert("This page is created by ujjwal")
+const student = {
+    Name :"Rajan",
+    Class :"10",
+    Roll :"21",
+    marks :"102",
+    isPass :"true",
+}
+student.Name = student.Name+" Hello"
+console.log(student.Name)
+console.log(typeof student.Name)
